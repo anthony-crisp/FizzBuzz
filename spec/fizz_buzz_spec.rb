01c1d69916b4b95f7a0d68e392434ae9fb1038c1
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-04-06 17:26:31 +0100

    Passed third test

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -3,9 +3,12 @@ require 'fizz_buzz'
 describe 'fizz_buzz'do
   it "should return 'Fizz' if passed 3" do
     expect(fizz_buzz(3)).to eq "Fizz"
-end
+  end
   it "should return 'Buzz' if passed 5" do
     expect(fizz_buzz(5)).to eq "Buzz"
+  end
+  it "should return 'FizzBuzz'if passed 15" do
+  expect(fizz_buzz(15)).to eq "FizzBuzz"
   end
 
 end
