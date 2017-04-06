7f39734ce29f05a2e6512ba6366a55c4e3b89f7e
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-04-06 17:17:19 +0100

    Passed first test

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,5 @@
+def fizz_buzz(n)
+  if n % 3 == 0
+    "Fizz"
+  end
+end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,7 +2,7 @@ require 'fizz_buzz'
 
 describe 'fizz_buzz'do
   it "should return 'Fizz' if passed 3" do
-    expect (fizz_buzz(3)).to eq "Fizz"
+    expect(fizz_buzz(3)).to eq "Fizz"
 end
 
 end
