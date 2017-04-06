d90ea5893b99005e648a8e381c2f1041617e743b
Anthony Crisp <anthony_crisp@hotmail.comp>
2017-04-06 17:21:09 +0100

    Passed second test

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,5 +1,7 @@
 def fizz_buzz(n)
   if n % 3 == 0
     "Fizz"
+  elsif n % 5 == 0
+    "Buzz"
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,5 +4,8 @@ describe 'fizz_buzz'do
   it "should return 'Fizz' if passed 3" do
     expect(fizz_buzz(3)).to eq "Fizz"
 end
+  it "should return 'Buzz' if passed 5" do
+    expect(fizz_buzz(5)).to eq "Buzz"
+  end
 
 end
